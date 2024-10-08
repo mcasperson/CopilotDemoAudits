762f55659da727372f2639f315c7684b4658dd98
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-09 09:33:36 +1000

    reverted changes

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/new', (req, res) => {
-    res.send(null.getMessage()) 
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
