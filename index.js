565d0782bf33602484ccd6b2ac6ea7d784a28dfc
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-28 12:05:16 +1000

    added endpoint

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/null', (req, res) => {
+    null.send('Hello World!')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
